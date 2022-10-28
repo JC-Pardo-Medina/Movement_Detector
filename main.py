4493c5d754d8f33599b54e508aecc3ebcd908f73
JC-Pardo-Medina <jcparmed@gmail.com>
2022-10-28 23:39:17 +0200

    Time that has to pass feature

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def movement_detector(carpeta,carpetanueva):
     contrast = float(contrast_grid.get())
     brightness = int(brightness_grid.get())
     speed = float(speed_grid.get())
+    time_that_has_to_pass = float(time_that_has_to_pass_grid.get())
     with open(f'{carpetanueva}/{file_title}.txt', 'w') as f:
         for video in os.listdir(carpeta):
             f.write(f"{video}:\n")
@@ -34,7 +35,10 @@ def movement_detector(carpeta,carpetanueva):
                     break
                 if firstFrame is None:
                     frames -= 1
+                    last_message = False
+                    time_without_change = 0
                 frames += 1
+                time_without_change +=1
                 if (frames-1) % (fps * speed) == 0:
                     frame = imutils.resize(frame, width=500)
                     new_image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -65,10 +69,13 @@ def movement_detector(carpeta,carpetanueva):
                         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
                         owlIs = True
                     if owlIs != owlWas:
-                        if owlIs == True:
+                        time_without_change = 0
+                        if (owlIs == True) and (last_message == False):
                             f.write(f"Empieza a haber actividad: {time}\n")
-                        else:
-                            f.write(f"Deja de haber actividad: {time}\n") 
+                            last_message = True
+                    if (owlIs == False) and (time_without_change > fps * time_that_has_to_pass) and (last_message == True):
+                        f.write(f"Deja de haber actividad: {time}\n")
+                        last_message = False
                     owlWas = owlIs
             vs.release()
             f.write(f"Acaba el vídeo: {time}\n\n")
@@ -97,15 +104,17 @@ brightness_grid_position = (3,0)
 brightness_grid_label_position = (2,0)
 contrast_grid_position = (5,0)
 contrast_grid_label_position = (4,0)
-speed_grid_position = (5,1)
-speed_grid_label_position = (4,1)
-button_videos_folder_position = (0,1)
-button_videos_folder_label_position = (1,1)
-button_txt_folder_position = (2,1)
-button_txt_folder_label_position = (3,1)
+speed_grid_position = (5,2)
+speed_grid_label_position = (4,2)
+button_videos_folder_position = (0,2)
+button_videos_folder_label_position = (1,2)
+button_txt_folder_position = (2,2)
+button_txt_folder_label_position = (3,2)
 min_area_grid_position = (7,0)
 min_area_grid_label_position = (6,0)
-main_button_position = (7,1)
+time_that_has_to_pass_grid_position = (7,2)
+time_that_has_to_pass_grid_label_position = (6,2)
+main_button_position = (8,1)
 padx=5
 pady=5
 window = Tk()
@@ -144,6 +153,11 @@ min_area_grid = ttk.Entry()
 min_area_label = ttk.Label(text="Área mínima")
 min_area_label.grid(row=min_area_grid_label_position[0],column=min_area_grid_label_position[1],padx=padx,pady=pady)
 min_area_grid.grid(row=min_area_grid_position[0],column=min_area_grid_position[1],padx=padx,pady=pady)
+time_that_has_to_pass_grid = ttk.Entry()
+time_that_has_to_pass_label = ttk.Label(text="Tiempo que debe pasar sin actividad para que cuente que el búho se ha ido")
+time_that_has_to_pass_label.grid(row=time_that_has_to_pass_grid_label_position[0],column=time_that_has_to_pass_grid_label_position[1],padx=padx,pady=pady)
+time_that_has_to_pass_grid.grid(row=time_that_has_to_pass_grid_position[0],column=time_that_has_to_pass_grid_position[1],padx=padx,pady=pady)
+time_that_has_to_pass_grid.insert(END,0)
 min_area_grid.insert(END,500)
 main_button = ttk.Button(text="Ejecutar",command=lambda: movement_detector(old_folder_path,new_folder_path))#centro
 main_button.grid(row=main_button_position[0],column=main_button_position[1],padx=padx,pady=pady)
